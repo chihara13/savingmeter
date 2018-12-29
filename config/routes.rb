6120383383b5d9776fd28d12549501f1a2531ee1
chihara <bass_love_mini@ybb.ne.jp>
2018-12-30 02:12:49 +0900

    first-commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'home/index'
+  # devise_for :users
+  root to: 'home#index'
+  resources :notes
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+  # get 'about/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
